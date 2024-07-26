2c14d5ca175f78b7891d20a01fa8e3e23ac00a8e
karn chaudhary <karn.2302142@sanskriti.ac.in>
2024-07-26 08:46:20 +0530

    Add files via upload

---
+++ Greeting MessageGenerator.py
@@ -0,0 +1,9 @@
+
+# Get user input
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Generate greeting message
+greeting_message = f"Hello, {first_name} {last_name}! You just developed into Python."
+
+print(  greeting_message )
